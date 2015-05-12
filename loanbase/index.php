decafd9adec32856da50b3af193826b8a6b03118
kennj87 <mail@kennj.com>
2015-05-12 13:41:14 +0200

    Update index.php
    Formating and validating.

--- loanbase/index.php
+++ loanbase/index.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <html>
+<title>ITEK14A BorrowBase</title>
 <body>
-
 <h1>Borrow base</h1>
 <p>Users are lending Items right here.</p>
-</p>
-<h2> Create </h2></p>
-<a href="users.php">New Users</a> </p>
-<a href="items.php">New Items</a> </p>
-<a href="new_loans.php">New Loans</a> </p>
-<a href="old_loans.php">Old Loans</a> </p>
+<h2> Create </h2>
+<p><a href="users.php">New Users</a> </p>
+<p><a href="items.php">New Items</a> </p>
+<p><a href="new_loans.php">New Loans</a> </p>
+<p><a href="old_loans.php">Old Loans</a> </p>
 </body>
 </html>
 <?php include 'footer.php';?>
